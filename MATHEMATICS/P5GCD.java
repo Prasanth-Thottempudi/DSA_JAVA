919afe97f86cbb38169123187229fc012392e655
prasanth <tprasanth678@gmail.com>
2024-12-17 07:54:26 +0530

    gcd using euclids algorithm

--- MATHEMATICS/P5GCD.java
+++ MATHEMATICS/P5GCD.java
@@ -1,19 +1,15 @@
 public class P5GCD {
 
+    static int gcdUsingEucliedsAlg(int a,int b){
+        if(b==0) return a;
+        return gcdUsingEucliedsAlg(b,a%b);
+    }
     public static void main(String[] args) {
 
         int a=10;
         int b=15;
-        int min;
-        int gcd=1;
-        if(a<b) {
-            min = a;
-        }else{
-            min=b;
-        }
-        for(int i=min;i>=1;i--){
-            if(a%i==0 && b%i==0) gcd=i; break;
-        }
+        int gcd=gcdUsingEucliedsAlg(a,b);
         System.out.print(gcd);
+
     }
 }
